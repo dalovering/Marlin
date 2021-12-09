b33123d378661cb9fde44b406bd70d1b7de5ca87
Dean Lovering <dalovering@gmail.com>
2021-12-09 17:47:16 -0500

    Update machine parameters
    Updated probe offsets, backlash correction, and E steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -917,7 +917,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT  { 80.25, 80.25, 400, 100.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT  { 80.25, 80.25, 400, 93.6 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1169,7 +1169,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -9, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -9, -0.55 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
